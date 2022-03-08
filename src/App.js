ddd65178b001f56ac8765f68d04a85c92b3cd447
gmusita30 <gabrieladrian.usita.iics@ust.edu.ph>
2022-03-08 22:38:21 +0800

    publish

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 function App() {
   return (
   <>
-    <BrowserRouter>
+
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={ 
         <>
